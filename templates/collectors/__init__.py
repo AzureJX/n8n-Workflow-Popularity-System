b8473caa97f71af79e2fb61886ba135a3dde8233
AzureJX <jwxu1118@gmail.com>
2025-09-15 00:50:53 -0400

    Delete templates/collectors directory

--- templates/collectors/__init__.py
+++
@@ -1,5 +0,0 @@
-from .youtube_collector import collect_youtube
-from .discourse_collector import collect_discourse
-from .trends_collector import collect_trends
-
-__all__ = ["collect_youtube", "collect_discourse", "collect_trends"]
