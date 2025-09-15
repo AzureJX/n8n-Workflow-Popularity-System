1099faccbad5a38916f12a1b6e14dea4859459ce
AzureJX <jwxu1118@gmail.com>
2025-09-15 00:48:55 -0400

    Create __init__.py

---
+++ collectors/__init__.py
@@ -0,0 +1,5 @@
+from .youtube_collector import collect_youtube
+from .discourse_collector import collect_discourse
+from .trends_collector import collect_trends
+
+__all__ = ["collect_youtube", "collect_discourse", "collect_trends"]
